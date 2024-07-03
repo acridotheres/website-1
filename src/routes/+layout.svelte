466c33cc8ef14cb4db913f562dbfb972b758c677
Leonard Lesinski <84378319+Le0X8@users.noreply.github.com>
2024-07-03 22:08:24 +0200

    Added some styling

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import '../styles/global.scss';
+</script>
+
+<div class="bg-black h-12 w-full"></div>
+
+<nav class="fixed top-0 left-0 backdrop-blur-lg w-full h-12 border-b border-b-neutral-800 flex text-white items-center">
+	<a href="/" class="flex p-1 items-center w-fit">
+		<img src="/logo.png" alt="Acridotheres logo" class="h-10 mr-2">
+		<h1 class="text-bold text-2xl">Acridotheres</h1>
+	</a>
+	<div class="flex flex-grow items-center justify-end h-full p-2 px-3">
+		<a class="bg-white text-black h-full leading-6 p-1 px-2 rounded-md" href="/download">Download</a>
+	</div>
+</nav>
+
+<main class="bg-black text-white w-full min-h-[calc(100vh-3rem)]">
+	<slot />
+</main>
+
+<footer>
+	<p class="text-center text-white bg-black p-2">
+		&copy; 2024 Leonard Lesinski and all Acridotheres contributors
+	</p>
+</footer>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="main">
+  <img src="/logo.png" alt="Acridotheres logo" class="r logo">
+  <h1 class="l">
+    This is<br>
+    <span class="fancy">Acridotheres</span>.
+  </h1>
+  <h2 class="l clear-left">
+    a very fast, modern and open-source archiving tool. written in rust.
+  </h2>
+
+  <div class="b w-full h-32 border border-neutral-800 rounded-md gui">
+    <div class="flex items-center w-full h-8 p-2 border-b border-neutral-800">
+      <div class="flex-grow"></div>
+      <div class="flex gap-2">
+        <div class="size-4 bg-green-500 rounded-full"></div>
+        <div class="size-4 bg-yellow-500 rounded-full"></div>
+        <div class="size-4 bg-red-500 rounded-full"></div>
+      </div>
+    </div>
+  </div>
+</div>
